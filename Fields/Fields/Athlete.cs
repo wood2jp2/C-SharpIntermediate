7eb9261d95f1e74452a55daa273cfaf747312613
Josh Wood <wood2jp2@gmail.com>
2018-08-24 13:23:01 -0400

    adding Fields stuffs

---
+++ Fields/Fields/Athlete.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Fields
+{
+    public class Athlete
+    {
+        public string Sport;
+
+        public int Number;
+
+        public string Position;
+
+        public string Team;
+
+    }
+}
---
+++ Fields/Fields/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Fields
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            var Josh = new Athlete() { Sport = "Baseball", Position = "2 base" };
+            List<Athlete> Athletes = new List<Athlete>() { Josh };
+            Athletes.Add(new Athlete() { Number = 42, Sport="Basketball", Position="Guard"});
+            Console.WriteLine(Athletes[1].Sport);
+        }
+    }
+}
