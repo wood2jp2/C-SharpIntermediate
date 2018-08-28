8c0766943dfd13c7c265e70328b27989bda0ba44
Josh Wood <wood2jp2@gmail.com>
2018-08-27 22:50:13 -0400

    finished getters and setters on properties

---
+++ Properties/Properties/Person.cs
@@ -0,0 +1,62 @@
+﻿using System;
+namespace Properties
+{
+    public class Person
+    {
+
+        //private DateTime _birthdate;
+
+        // LONG WAY TO DECLARE - getters and setters
+
+        //public DateTime GetBirthdate() {
+        //    return _birthdate;
+        //}
+
+        //public void SetBirthdate(DateTime birthdate) {
+        //    _birthdate = birthdate;
+        //}
+
+        // SHORT WAY TO DECLARE - getters and setters
+
+            //public DateTime Birthdate {
+            //get { return _birthdate; }
+            //set { _birthdate = value; }
+            //}
+           
+        // EVEN SHORTER WAY TO DECLARE - getters and setters
+
+        public DateTime Birthdate { get; private set; } // C# compiler automatically generates a private field here
+        // with the 'private' set, you now cannot set the Birthdate inside the code, and must use ctor
+
+        public Person(DateTime birthdate)
+        {
+            Birthdate = birthdate;
+        }
+
+
+        public int Age {
+            get {
+                var timeSpan = DateTime.Today - Birthdate;
+                var years = timeSpan.Days / 365;
+                return years;
+            }
+            // no 'set' method since the Age is being calculated and should not be set manually
+
+        }
+
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        // type faster properties with 'prop' + tab
+
+        public string UserName
+        {
+            get;
+            set;
+        }
+
+    }
+}
---
+++ Properties/Properties/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Properties
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var josh = new Person(new DateTime(1992, 11, 12));
+            //josh.Birthdate = new DateTime(1992, 11, 12);can't use this code with private set
+
+            Console.WriteLine(josh.Age);
+        }
+    }
+}
