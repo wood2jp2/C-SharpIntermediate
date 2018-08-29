43b344edb46c11c908df8922c470587ca1fa9528
Wood <Joshua.Wood@hhs.gov>
2018-08-29 18:01:14 -0400

    finished exercise 1

---
+++ ListTests/Fields/Athlete.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace ListTests
+{
+    public class Athlete
+    {
+        public string Sport;
+
+        public int Number;
+
+        public string Position;
+
+        public string Team;
+
+    }
+}
---
+++ ListTests/Fields/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListTests
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            var Josh = new Athlete() { Sport = "Baseball", Position = "2 base" };
+            var Athletes = new List<Athlete>() { Josh };
+            Athletes.Add(new Athlete() { Number = 42, Sport="Basketball", Position="Guard"});
+
+            Console.WriteLine(Athletes[1].Sport);
+        }
+    }
+}
