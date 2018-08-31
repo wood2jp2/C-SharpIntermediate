c29a4ffe40922b4cc6614996b4f373941f5ce056
Josh Wood <wood2jp2@gmail.com>
2018-08-31 00:08:53 -0400

    completed second exercise

---
+++ Exercises/S2E2-DesignaStackOverflowPost/S2E2-DesignaStackOverflowPost/Post.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace S2E2DesignaStackOverflowPost
+{
+    public class Post
+    {
+        int Votes { get; set; } = 0;
+        string Title { get; set; }
+        string Description { get; set; }
+        DateTime CreatedAt { get; } = DateTime.Now;
+
+        public Post()
+        {
+        }
+
+        public Post(string title) : this() {
+            Title = title;
+        }
+
+        public Post(string title, string description) : this(title) {
+            Description = description;
+        }
+
+        public void CastVote(string vote)
+        {
+            if (vote == "upvote"){
+                Votes += 1;
+            }
+            else if (vote == "downvote") {
+                Votes -= 1;
+            }
+            Console.WriteLine(Votes);
+        } 
+    }
+}
---
+++ Exercises/S2E2-DesignaStackOverflowPost/S2E2-DesignaStackOverflowPost/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace S2E2DesignaStackOverflowPost
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var firstPost = new Post();
+            firstPost.CastVote("upvote");
+            firstPost.CastVote("upvote");
+            firstPost.CastVote("downvote");
+
+            var secondPost = new Post("Fifty Miles");
+            // secondPost.CreatedAt = DateTime.Today; You cannot reassign since there is no 'set'.
+
+            var thirdPost = new Post("Men In Black", "movie description");
+            Console.WriteLine(secondPost.CreatedAt);
+        }
+    }
+}
