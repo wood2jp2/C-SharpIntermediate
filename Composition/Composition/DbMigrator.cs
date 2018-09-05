9a1a05c06391d75bc7994426530f5510d36b26e9
Josh Wood <joshua.wood@hhs.gov>
2018-09-05 13:29:29 -0400

    completed composition section

---
+++ Composition/Composition/DbMigrator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composition
+{
+    class DbMigrator
+    {
+        private readonly Logger _logger;
+
+        // if you hover over 'logger' in the arg, and press Alt+Enter, you can initialize a field for logger
+        public DbMigrator(Logger logger)
+        {
+            _logger = logger;
+        }
+
+        public void Migrate()
+        {
+            _logger.Log("we are migrating");
+        }
+    }
+}
---
+++ Composition/Composition/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // to pass a class type to another class: initialize like this OR set an instance to a variable (var something = new Something())
+            var dbMigrator = new DbMigrator(new Logger());
+
+            var logger = new Logger();
+            var installer = new Installer(logger);
+
+            // the take away here is that we are able to compose classes (migrator and installer) that can take in a logger
+            // instance, and use its' logging function to log something, all within the Migrate and Install methods
+            dbMigrator.Migrate();
+            installer.Install();
+        }
+    }
+}
