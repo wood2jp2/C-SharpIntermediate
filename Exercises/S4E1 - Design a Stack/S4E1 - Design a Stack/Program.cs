9dd01a06da0886fc3a454fa0b7c564845127c6e3
Josh Wood <joshua.wood@hhs.gov>
2018-09-10 14:08:41 -0400

    finished exercise for Section 4

---
+++ Exercises/S4E1 - Design a Stack/S4E1 - Design a Stack/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S4E1___Design_a_Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack();
+            stack.Push(999);
+            stack.Push(888);
+            stack.Push(777);
+
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+
+        }
+    }
+}
---
+++ Exercises/S4E1 - Design a Stack/S4E1 - Design a Stack/Stack.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S4E1___Design_a_Stack
+{
+    public class Stack
+    {
+        private int Position = -1;
+        private ArrayList StackItems = new ArrayList();
+
+        public void Push (object obj)
+        {
+            if (obj != null)
+            {
+                Position += 1;
+                StackItems.Add(obj);
+            } else
+            {
+                throw new InvalidOperationException();
+            }
+        }
+
+        public object Pop()
+        {
+            var returnable = StackItems[StackItems.Count-1];
+            StackItems.RemoveAt(StackItems.Count - 1); 
+            return returnable;
+        }
+
+        public void Clear()
+        {
+            StackItems.Clear();
+        }
+
+    }
+}
